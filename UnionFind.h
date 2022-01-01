2815c5840a27d9ee65e34af99729cff7dfa55d2c
nircoe <nircoe@gmail.com>
2022-01-01 19:18:37 +0200

    unionfind

--- UnionFind.h
+++ UnionFind.h
@@ -81,9 +81,10 @@ public:
             root_i = root_i->GetParent();
         while (root_j->GetParent() != nullptr)
             root_j = root_j->GetParent();
-        if(root_i == root_j) throw std::exception();
+        if(root_i == root_j) throw std::exception(); // maybe our own exception
         T t;
-        if (root_i->GetTreeSize() >= root_j->GetTreeSize())
+        int i_size = root_i->GetTreeSize(), j_size = root_j->GetTreeSize(), new_size = i_size + j_size;
+        if (i_size >= J_size)
         {
             root_j->SetParent(root_i);
             t = root_i->GetData();
@@ -93,7 +94,6 @@ public:
             root_i->SetParent(root_j);
             t = root_j->GetData();
         }
-        int new_size = root_i->GetTreeSize() + root_j->GetTreeSize();
         root_i->SetTreeSize(new_size);
         root_j->SetTreeSize(new_size);
         return t;
